52bd04d5f2b3b9b2beeaeaf38bf1890e7a53ce1a
Paula Helena Câmara <127454057+Paula-260@users.noreply.github.com>
2023-04-03 23:27:37 -0300

    Add files via upload

---
+++ Hello GitHub/src/br/com/paula/git/HelloGit.java
@@ -0,0 +1,18 @@
+/**
+ * 
+ */
+package br.com.paula.git;
+
+/**
+ * @author Paula
+ *Exemplo de integração do github
+ */
+public class HelloGit {
+
+	
+	public static void main(String[] args) {
+		System.out.println("Hello GitHub ");
+
+	}
+
+}
